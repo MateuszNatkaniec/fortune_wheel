302db93173957c5e6b90e0428b42d36d78a220ec
Mateusz Natkaniec <mateusznatkaniec711@gmail.com>
2021-05-24 18:50:51 +0200

    state for players and update score functionality

---
+++ players.py
@@ -0,0 +1,39 @@
+players = [
+    {
+        "name": "Janusz",
+        "score": 0,
+        "is_active": True
+    },
+    {
+        "name": "Ela",
+        "score": 0,
+        "is_active": False
+    },
+    {
+        "name": "Brian",
+        "score": 0,
+        "is_active": False
+    }
+]
+
+
+def get_active_player():
+    for player in players:
+        if player['is_active']:
+            return player
+
+
+def update_score(amount):
+    active_player = get_active_player()
+
+
+    if amount > 0:
+        active_player["score"] += amount
+    else:
+        active_player["score"] = 0
+
+update_score(400)
+update_score(200)
+update_score(0)
+
+print(get_active_player())
